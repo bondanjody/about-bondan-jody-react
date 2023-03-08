278bddc3ab9644ae7b95d4b5678b5af1a341e79d
bondanjody <bondanjodys@gmail.com>
2023-03-08 19:02:57 +0700

    Menambahkan fitur responsif ke dalam section Education

--- src/assets/components/Layout/Education.jsx
+++ src/assets/components/Layout/Education.jsx
@@ -1,21 +1,21 @@
 const Education = props => {
-    return <div class="mt-4 bg-slate-600 py-4">
-                <h2 class="text-2xl text-center text-white mb-4">Education</h2>
-                <div>
-                    <div class="w-3/4 mx-auto border border-orange-400 rounded-xl overflow-hidden">
-                        <div class="bg-orange-400 p-4 text-center font-semibold">
+    return <div className="mt-4 bg-slate-600 py-4">
+                <h2 className="text-2xl text-center text-white mb-4">Education</h2>
+                <div className="md:flex md:justify-center md:px-2 md:items-center">
+                    <div className="w-3/4 mx-auto border border-orange-400 rounded-xl overflow-hidden md:w-1/3 md:h-44">
+                        <div className="bg-orange-400 p-4 text-center font-semibold">
                             <h4>SMKN 1 Bulukerto</h4>
                         </div>
-                        <div class="p-4 text-white text-sm">
+                        <div className="p-4 text-white text-sm">
                             <p>🕰️ 2015 - 2018</p>
                             <p>📚 Software Engineering</p>
                         </div>
                     </div>
-                    <div class="mt-2 w-3/4 mx-auto border border-orange-400 rounded-xl overflow-hidden">
-                        <div class="bg-orange-400 p-4 text-center font-semibold">
+                    <div className="mt-2 w-3/4 mx-auto border border-orange-400 rounded-xl overflow-hidden md:w-1/3 md:h-44 md:mt-0">
+                        <div className="bg-orange-400 p-4 text-center font-semibold">
                             <h4>Universitas Bina Sarana Informatika</h4>
                         </div>
-                        <div class="p-4 text-white text-sm">
+                        <div className="p-4 text-white text-sm">
                             <p>🕰️ 2021 - now (Semester 4)</p>
                             <p>📚 Computer Science</p>
                         </div>
