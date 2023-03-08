c37bcd2a63580590fae4c1cec90c8baed10286d9
bondanjody <bondanjodys@gmail.com>
2023-03-08 13:29:52 +0700

    Menambahkan gambar-gambar. Membuat component Header dan Introduction

--- src/assets/components/Layout/Header.jsx
+++ src/assets/components/Layout/Header.jsx
@@ -1,5 +1,7 @@
 const Header = props => {
-
+    return  <div class="p-4 border-b shadow-lg">
+                <h1 class="text-2xl font-bold text-slate">Hello, welcome !</h1>
+            </div>
 }
 
 export default Header;
--- src/assets/components/Layout/Introduction.jsx
+++ src/assets/components/Layout/Introduction.jsx
@@ -1,5 +1,15 @@
-const Introduction = props => {
+import pasFotoPic from '../../images/pas-foto.jpg';
 
+const Introduction = props => {
+    return  <div class="mt-4 w-full">
+                <div class="container mx-auto w-80 h-80 rounded-full bg-cover" style={{backgroundImage:`url(${pasFotoPic})`}}>
+                </div>
+                {/* Description */}
+                <div class="p-4 text-lg">
+                    <p>My name is <span class="underline font-semibold">Bondan Jody Setyawan</span>. I am 23 years-old.</p>
+                    <p>And I am a 🌐 Web Developer focusing on Fronted. </p>
+                </div>
+            </div>
 }
 
 export default Introduction;
