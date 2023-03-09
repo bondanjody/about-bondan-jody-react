c8290e6bf3ea7783226a72403e06352743a3b0f1
bondanjody <bondanjodys@gmail.com>
2023-03-09 15:51:29 +0700

    Menambahkan progress bar pada tiap skills

--- src/assets/components/Layout/Skills.jsx
+++ src/assets/components/Layout/Skills.jsx
@@ -14,77 +14,86 @@ const Skills = props => {
                 {/* Skills container */}
                 <div className="mt-2 lg:px-16">
                     <div className="p-4 h-16 border rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
-                        <img src={htmlPic} alt={htmlPic} className="w-10 inline-block mr-4" />   
-                        <p>HTML</p>
+                        <img src={htmlPic} alt={htmlPic} className="w-10 inline-block mr-4" /> 
+                        <div className='w-full'>
+                            <p>HTML</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '100%'}}> 100%</div>
+                            </div>
+                        </div>  
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={cssPic} alt={cssPic} className="w-10 inline-block mr-4" />   
-                        <p>CSS</p>
+                        <div className='w-full'>
+                            <p>CSS</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '95%'}}> 95%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={tailwindPic} alt={tailwindPic} className="w-10 inline-block mr-4" />   
-                        <p>Tailwind CSS</p>
+                        <div className='w-full'>
+                            <p>Tailwind CSS</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '85%'}}> 85%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={jsPic} alt={jsPic} className="w-10 inline-block mr-4" />   
-                        <p>JavaScript</p>
+                        <div className='w-full'>
+                            <p>JavaScript</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '92%'}}> 92%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={phpPic} alt={phpPic} className="w-10 inline-block mr-4" />   
-                        <p>PHP</p>
+                        <div className='w-full'>
+                            <p>PHP</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '90%'}}> 90%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={mysqlPic} alt={mysqlPic} className="w-10 inline-block mr-4" />   
-                        <p>MySQL</p>
+                        <div className='w-full'>
+                            <p>MySQL</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '78%'}}> 78%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={pythonPic} alt={pythonPic} className="w-10 inline-block mr-4" />   
-                        <p>Python</p>
+                        <div className='w-full'>
+                            <p>Python</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '88%'}}> 88%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={gitPic} alt={gitPic} className="w-10 inline-block mr-4" />   
-                        <p>Git and GitHub</p>
+                        <div className='w-full'>
+                            <p>Git and GitHub</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '90%'}}> 90%</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="p-4 h-16 border mt-2 rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={linuxPic} alt={linuxPic} className="w-10 inline-block mr-4" />   
-                        <p>Linux</p>
+                        <div className='w-full'>
+                            <p>Linux</p>
+                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '82%'}}> 82%</div>
+                            </div>
+                        </div>
                     </div>
-                    {/* <div className="p-4 h-16 border rounded-lg w-96 flex items-center hover:cursor-pointer">
-                        <img src={htmlPic} alt={htmlPic} className="w-10 inline-block mr-4" />   
-                        <p>HTML</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={cssPic} alt={cssPic} className="w-10 inline-block mr-4" />   
-                        <p>CSS</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={tailwindPic} alt={tailwindPic} className="w-10 inline-block mr-4" />   
-                        <p>Tailwind CSS</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={jsPic} alt={jsPic} className="w-10 inline-block mr-4" />   
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={phpPic} alt={phpPic} className="w-10 inline-block mr-4" />   
-                        <p>PHP</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={mysqlPic} alt={mysqlPic} className="w-10 inline-block mr-4" />   
-                        <p>MySQL</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={pythonPic} alt={pythonPic} className="w-10 inline-block mr-4" />   
-                        <p>Python</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={gitPic} alt={gitPic} className="w-10 inline-block mr-4" />   
-                        <p>Git and GitHub</p>
-                    </div>
-                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
-                        <img src={linuxPic} alt={linuxPic} className="w-10 inline-block mr-4" />   
-                        <p>Linux</p>
-                    </div> */}
                 </div>
             </div>
 }
