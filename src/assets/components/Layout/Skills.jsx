14028f707bacdf9bed007961d4da6b31f8ad9071
bondanjody <bondanjodys@gmail.com>
2023-03-09 15:39:01 +0700

    Mengubah tampilan tiap elemen Skills ketika layar desktop

--- src/assets/components/Layout/Skills.jsx
+++ src/assets/components/Layout/Skills.jsx
@@ -12,7 +12,7 @@ const Skills = props => {
     return <div className="w-full mt-2">
                 <h2 className="border-l-4 border-teal-500 px-2 text-xl font-semibold">Skills</h2>
                 {/* Skills container */}
-                <div className="mt-2 p-2">
+                <div className="mt-2 lg:px-16">
                     <div className="p-4 h-16 border rounded-lg flex items-center hover:bg-slate-200 hover:shadow-lg hover:cursor-pointer">
                         <img src={htmlPic} alt={htmlPic} className="w-10 inline-block mr-4" />   
                         <p>HTML</p>
@@ -49,6 +49,42 @@ const Skills = props => {
                         <img src={linuxPic} alt={linuxPic} className="w-10 inline-block mr-4" />   
                         <p>Linux</p>
                     </div>
+                    {/* <div className="p-4 h-16 border rounded-lg w-96 flex items-center hover:cursor-pointer">
+                        <img src={htmlPic} alt={htmlPic} className="w-10 inline-block mr-4" />   
+                        <p>HTML</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={cssPic} alt={cssPic} className="w-10 inline-block mr-4" />   
+                        <p>CSS</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={tailwindPic} alt={tailwindPic} className="w-10 inline-block mr-4" />   
+                        <p>Tailwind CSS</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={jsPic} alt={jsPic} className="w-10 inline-block mr-4" />   
+                        <p>JavaScript</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={phpPic} alt={phpPic} className="w-10 inline-block mr-4" />   
+                        <p>PHP</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={mysqlPic} alt={mysqlPic} className="w-10 inline-block mr-4" />   
+                        <p>MySQL</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={pythonPic} alt={pythonPic} className="w-10 inline-block mr-4" />   
+                        <p>Python</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={gitPic} alt={gitPic} className="w-10 inline-block mr-4" />   
+                        <p>Git and GitHub</p>
+                    </div>
+                    <div className="p-4 h-16 border rounded-lg w-1/2 flex items-center hover:cursor-pointer">
+                        <img src={linuxPic} alt={linuxPic} className="w-10 inline-block mr-4" />   
+                        <p>Linux</p>
+                    </div> */}
                 </div>
             </div>
 }
