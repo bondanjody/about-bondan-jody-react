89ae4a051f79f03e06bedaf5fc86899373672107
bondanjody <bondanjodys@gmail.com>
2023-03-10 10:24:15 +0700

    Menambahkan komponen Languages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Fragment} from 'react'
 import Introduction from './assets/components/Layout/Introduction'
 import Skills from './assets/components/Layout/Skills'
 import Education from './assets/components/Layout/Education'
+import Languages from './assets/components/Layout/Languages'
 
 function App() {
 
   return  <Fragment>
             <Introduction />
             <Skills />
+            <Languages />
             <Education />
           </Fragment>
 }
---
+++ src/assets/components/Layout/Languages.jsx
@@ -0,0 +1,71 @@
+const Languages = () => {
+    return <div className="mt-4 bg-slate-300 py-4">
+                <h2 className="text-2xl text-center text-slate-800 mb-4">Languages</h2>
+                <div className="md:flex md:justify-center md:px-2 md:items-center">
+                    <div className="w-3/4 mx-auto border border-slate-800 rounded-xl overflow-hidden md:w-1/3 md:h-auto">
+                        <div className="bg-slate-800 text-white p-4 text-center font-semibold">
+                            <h4>English</h4>
+                        </div>
+                        <div className="p-4 text-slate-800 text-sm">
+                            <div className='w-full'>
+                                <p>Writing</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '87%'}}> 87%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Speaking</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '85%'}}> 85%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Listening</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '89%'}}> 89%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Reading</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '90%'}}> 90%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-2 w-3/4 mx-auto border border-slate-800 rounded-xl overflow-hidden md:w-1/3 md:h-auto md:mt-0">
+                        <div className="bg-slate-800 text-white p-4 text-center font-semibold">
+                            <h4>Bahasa Indonesia</h4>
+                        </div>
+                        <div className="p-4 text-slate-800 text-sm">
+                            <div className='w-full'>
+                                <p>Writing</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '100%'}}> 100%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Speaking</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '100%'}}> 100%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Listening</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '100%'}}> 100%</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <p>Reading</p>
+                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div class="bg-teal-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '100%'}}> 100%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
+}
+
+export default Languages;
