75218386106cfbd4a8b70460b8ed65a2dffa761f
bondanjody <bondanjodys@gmail.com>
2023-03-11 15:13:24 +0700

    Membuat komponen Certifications dapat menampilkan data sertifikat. Membuat komponen button next dan prev pada Certifications berfungsi.

--- src/assets/components/Layout/Certifications.jsx
+++ src/assets/components/Layout/Certifications.jsx
@@ -8,8 +8,12 @@ import devHacksCert from '../../images/sertifikat-developer-life-hacks-menggunak
 import dutaBangsa1 from '../../images/Sertifikatdutabangsa1.jpg';
 import dutaBangsa2 from '../../images/Sertifikatdutabangsa2.jpg';
 import flutterCert from '../../images/sertifikat-everything-you-need-to-know-about-flutter-forward-certificate-1.png';
+import { useState } from 'react';
 
 const Certifications = () => {
+    const [certificateIndex, setCertificateIndex] = useState(0);
+
+    // Certificate Data
     const certificateDatas = [
         {
             title: 'Belajar Dasar Pemrograman Javascript',
@@ -37,24 +41,40 @@ const Certifications = () => {
             pics: [flutterCert]
         }
     ]
+
+    // Button handler
+    const nextHandler = () => {
+        if (certificateIndex === certificateDatas.length-1) {
+            return setCertificateIndex(0);
+        }
+        setCertificateIndex(prevData => prevData+1);
+    }
+
+    const prevHandler = () => {
+        if (certificateIndex === 0) {
+            return setCertificateIndex(certificateDatas.length-1);
+        }
+        setCertificateIndex(prevData => prevData-1);
+    }
+
     return <div className="w-full mt-4 md:mt-2">
         <h2 className="border-l-4 border-teal-500 px-2 text-xl font-semibold mb-1 md:mb-2">Certifications</h2>
         <div className="mx-1 border rounded-md p-4">
             <div className='mb-2'>
-                <h3 className='text-2xl font-bold'>Certifications Title</h3>
-                <p className='text-sm'>Issued by Organization</p>
+                <h3 className='text-2xl font-bold'>{certificateDatas[certificateIndex].title}</h3>
+                <p className='text-sm'>Issued by {certificateDatas[certificateIndex].issuedBy}</p>
             </div>
             {/* Certification Image */}
             <div className='flex flex-col items-center mb-2'>
-                <img className='md:w-1/2' src={sertifikatPic} alt={`${sertifikatPic}.png`} />
+                <img className='md:w-1/2' src={certificateDatas[certificateIndex].pics[0]} alt={`${certificateDatas[certificateIndex].pics[0]}.png`} />
                 <p className='italic text-sm text-blue-700 hover:underline hover:cursor-pointer'>Click here to see the preview</p>
             </div>
             {/* Controll button */}
             <div className="flex justify-center">
-                <div className="w-10 h-10 mr-1 hover:cursor-pointer">
+                <div className="w-10 h-10 mr-1 hover:cursor-pointer" onClick={prevHandler}>
                     <img src={leftArrowPic} alt={`${leftArrowPic}.png`} />
                 </div>
-                <div className="w-10 h-10 hover:cursor-pointer">
+                <div className="w-10 h-10 hover:cursor-pointer" onClick={nextHandler}>
                     <img src={rightArrowPic} alt={`${rightArrowPic}.png`} />
                 </div>
             </div>
