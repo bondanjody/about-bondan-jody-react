8e6c11a5fbadeb8f965747d51373affa55127dce
bondanjody <bondanjodys@gmail.com>
2023-03-13 14:45:10 +0700

    Menambahkan padding pada Modal

--- src/assets/components/UI/Modal.jsx
+++ src/assets/components/UI/Modal.jsx
@@ -1,5 +1,5 @@
 const Modal = props => {
-    return <div className="fixed top-8 left-8 w-[calc(100%-4rem)] h-[calc(100vh-4rem)] overflow-x-scroll z-50 bg-white p-2">{props.children}</div>
+    return <div className="fixed top-8 left-8 w-[calc(100%-4rem)] h-[calc(100vh-4rem)] overflow-x-scroll z-50 bg-white py-4">{props.children}</div>
 }
 
 export default Modal;
