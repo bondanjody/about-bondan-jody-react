cc07e309a838957376f0176cd0e2ebe9f68a3ce3
bondanjody <bondanjodys@gmail.com>
2023-04-04 01:33:38 +0700

    Memberi transition pada komponen Introduction

--- src/assets/components/Layout/Introduction.jsx
+++ src/assets/components/Layout/Introduction.jsx
@@ -1,7 +1,7 @@
 import pasFotoPic from '../../images/pas-foto.png';
 
 const Introduction = props => {
-    return  <div className="md:m-4 md:container md:w-3/4 md:mx-auto bg-teal-700 shadow-lg shadow-slate-800 md:rounded-md p-2 flex md:justify-center">
+    return  <div className="md:m-4 md:container md:w-3/4 md:mx-auto bg-teal-700 shadow-md shadow-slate-500 md:rounded-md p-2 flex md:justify-center transition ease-in hover:scale-105 hover:shadow-lg hover:shadow-slate-800">
                 <div className='lg:w-1/3 lg:flex lg:justify-center'>
                     <img src={pasFotoPic} className='rounded-md' alt={`${pasFotoPic}.png`} />
                 </div>
